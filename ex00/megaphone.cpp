7e599ea14a65a34186c90b9308b9288c8f5186d6
romann-broque <kinoide56@gmail.com>
2023-06-16 11:55:54 +0200

    fix static empty fct

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:00:49 by rbroque           #+#    #+#             */
-/*   Updated: 2023/06/16 11:54:56 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/06/16 11:55:34 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ static std::string	toUpperString(std::string str)
 		str[i] = toupper(str[i]);
 	return (str);
 }
-
-static 
 
 int main(int ac, char **av)
 {
