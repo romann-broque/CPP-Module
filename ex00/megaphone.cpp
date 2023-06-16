b2c11a48d41d8f8633337412b323e7c12053251c
romann-broque <kinoide56@gmail.com>
2023-06-16 11:55:22 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,18 +6,36 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:00:49 by rbroque           #+#    #+#             */
-/*   Updated: 2023/06/08 17:15:49 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/06/16 11:54:56 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
-using namespace std;
+#define NO_ARG_PROVIDED	"* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
-int main()
+static std::string	toUpperString(std::string str)
 {
-    cout << "Hello world!" << endl;
-    cout << "wouah" << endl;
-    cout << "wouah" << endl;
-    return 0;
+	for (size_t i(0); i < str.length(); ++i)
+		str[i] = toupper(str[i]);
+	return (str);
+}
+
+static 
+
+int main(int ac, char **av)
+{
+	--ac;
+	++av;
+	if (ac == 0)
+		std::cout << NO_ARG_PROVIDED;
+	for (int i(0); i < ac; ++i)
+	{
+		std::string	arg(av[i]);
+		std::cout << toUpperString(arg);
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
 }
