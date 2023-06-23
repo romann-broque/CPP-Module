7dfdabdb4389d79f708d3dcbc3c84596a71b1e8d
romann-broque <kinoide56@gmail.com>
2023-06-23 15:48:34 +0200

    change arithmetic operator result to Fixed instances

--- Module_02/ex02/main.cpp
+++ Module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 12:05:22 by rbroque           #+#    #+#             */
-/*   Updated: 2023/06/22 16:05:05 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/06/23 15:47:25 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int	main()
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+
 	return (EXIT_SUCCESS);
 }
