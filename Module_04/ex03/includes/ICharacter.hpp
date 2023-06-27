44ccef7a238429c7bb928bbc9469198e3b8f1ff3
romann-broque <kinoide56@gmail.com>
2023-06-27 14:40:27 +0200

    ICharacter class

---
+++ Module_04/ex03/includes/ICharacter.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/27 14:33:00 by rbroque           #+#    #+#             */
+/*   Updated: 2023/06/27 14:35:24 by rbroque          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include "AMateria.hpp"
+
+class ICharacter
+{
+	public:
+
+		// Getter
+			virtual std::string const & getName() const = 0;
+		// Member functions
+			virtual void equip(AMateria* m) = 0;
+			virtual void unequip(int idx) = 0;
+			virtual void use(int idx, ICharacter& target) = 0;
+		// Destructor
+			virtual ~ICharacter() {}
+};
+
+#endif
