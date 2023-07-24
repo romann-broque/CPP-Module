f566a115f03fac0d4753185f480f34c61b49ec2c
romann-broque <kinoide56@gmail.com>
2023-07-24 16:06:42 +0000

    add test in main

--- Module_05/ex01/srcs/main.cpp
+++ Module_05/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 09:41:19 by rbroque           #+#    #+#             */
-/*   Updated: 2023/07/24 15:36:21 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/07/24 16:06:26 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int	main()
 {
 	Bureaucrat b1("Stanley", 127);
+	Bureaucrat b2("CEO", 1);
 
 	Form f1("Protocol 42", 13, 56);
 
@@ -27,4 +28,7 @@ int	main()
 	std::cout << f3 << std::endl;
 
 	b1.signForm(f1);
+	std::cout << f1 << std::endl;
+	b2.signForm(f1);
+	std::cout << f1 << std::endl;
 }
