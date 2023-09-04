63e1cf33f5cc9c82776813a8c7b89a8f246f59bd
romann-broque <kinoide56@gmail.com>
2023-09-04 12:49:48 +0000

    remove other Modules + pragma in ex03

--- Module_04/ex03/includes/IMateriaSource.hpp
+++ Module_04/ex03/includes/IMateriaSource.hpp
@@ -6,15 +6,15 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 00:32:21 by rbroque           #+#    #+#             */
-/*   Updated: 2023/06/28 00:45:59 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/04 12:48:58 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef IMATERIASOURCE_HPP
-# define IMATERIASOURCE_HPP
+#pragma once
 
-# include "AMateria.hpp"
-# define INVENTORY_SIZE	4
+#include "AMateria.hpp"
+
+#define INVENTORY_SIZE	4
 
 class IMateriaSource
 {
@@ -24,5 +24,3 @@ class IMateriaSource
 		virtual void learnMateria(AMateria*) = 0;
 		virtual AMateria* createMateria(std::string const & type) = 0;
 };
-
-#endif
