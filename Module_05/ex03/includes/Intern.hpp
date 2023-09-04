0467ad04d1eeb71fcfeb69d83f1ecd6b4a5a04b1
romann-broque <kinoide56@gmail.com>
2023-09-04 13:42:39 +0000

    ex03 pragma

--- Module_05/ex03/includes/Intern.hpp
+++ Module_05/ex03/includes/Intern.hpp
@@ -6,28 +6,30 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 09:33:47 by rbroque           #+#    #+#             */
-/*   Updated: 2023/07/25 13:14:14 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/09/04 13:41:30 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INTERN_HPP
-# define INTERN_HPP
+#pragma once
 
-# include <string>
-# include <iostream>
-# include "AForm.hpp"
-# include "PresidentialPardonForm.hpp"
-# include "ShrubberyCreationForm.hpp"
-# include "RobotomyRequestForm.hpp"
+#include <string>
+#include <iostream>
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
-# define RED		"\033[0;31m"
-# define GREEN		"\033[0;32m"
-# define NC			"\033[0m"
-# define FORM_KIND_COUNT 3
+// Colors
 
-# ifndef PRINT_DEBUG
-#  define PRINT_DEBUG true
-# endif
+#define RED			"\033[0;31m"
+#define GREEN		"\033[0;32m"
+#define NC			"\033[0m"
+
+#define FORM_KIND_COUNT 3
+
+#ifndef PRINT_DEBUG
+# define PRINT_DEBUG true
+#endif
 
 class Intern {
 
@@ -50,5 +52,3 @@ class Intern {
 		// Destructor
 			~Intern();
 };
-
-#endif
